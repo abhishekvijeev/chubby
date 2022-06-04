25bbc1a34325595f10ec2c84ff687d71e33fdebe
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2022-06-03 23:27:24 -0700

    Implement test to measure locks per unit time

--- chubby_client/src/lib.rs
+++ chubby_client/src/lib.rs
@@ -3,6 +3,9 @@ mod tests {
     use super::*;
     use chubby_server::constants::LockMode;
     use std::error::Error;
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
+    use std::time::{Duration, Instant};
 
     #[test]
     fn it_works() {
@@ -48,6 +51,44 @@ mod tests {
         let path = "/lock1";
         client.open(String::from(path)).await?;
         client.acquire(String::from(path), LockMode::SHARED).await?;
+        Ok(())
+    }
+
+    async fn acquire_lock(client_arc: Arc<Mutex<client::ChubbyClient>>, path_arc: Arc<Mutex<String>>) {
+        let mut client = client_arc.lock().await;
+        let path = path_arc.lock().await;
+        client.acquire(path.clone(), LockMode::EXCLUSIVE).await;
+    }
+
+    #[tokio::test()]
+    async fn test_locks_per_unit_time() -> Result<(), Box<(dyn Error + Send + Sync)>> {
+        let mut clients = Vec::new();
+        let mut lock_paths = Vec::new();
+        let mut thread_handles = Vec::new();
+        let num_locks = 1000;
+
+        for i in 1..num_locks {
+            let mut client = client::ChubbyClient::new().await?;
+            let path = format!("{}{}", "/lock/", i);
+            client.create_session(true).await?;
+            client.open(path.clone()).await?;
+            clients.push(Arc::new(Mutex::new(client)));
+            lock_paths.push(Arc::new(Mutex::new(path)));
+        }
+
+        let begin = Instant::now();
+        for i in 1..num_locks {
+            let shared_client = clients[i].clone();
+            let shared_path = lock_paths[i].clone();
+            thread_handles[i] = tokio::spawn(async move {
+                acquire_lock(shared_client, shared_path).await;
+            });
+        }
+        while thread_handles.len() > 0 {
+            let h = thread_handles.remove(0);
+            h.await;
+        }
+        println!("Time to acquire {} locks: {}ms", num_locks, begin.elapsed().as_millis());
         Ok(())
     }
 }
